4da04dbbca2496781f39635e66a25b8c82f816f8
folaaramide <afolabiaramide@gmail.com>
2025-08-13 13:03:36 +0100

    Create main.tf

---
+++ day-08/terraform-vpc-ipv6/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  required_version = ">= 1.3.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "nautilus" {
+  cidr_block                       = "10.0.0.0/16"
+  assign_generated_ipv6_cidr_block = true
+  enable_dns_support               = true
+  enable_dns_hostnames             = true
+  tags = {
+    Name = "nautilus-vpc"
+  }
+}
