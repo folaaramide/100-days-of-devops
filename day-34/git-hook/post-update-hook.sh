1c64bb6d0dd76e17045048440fe7f5a0030536e0
folaaramide <afolabiaramide@gmail.com>
2025-09-09 16:20:52 +0100

    Create post-update-hook.sh

---
+++ day-34/git-hook/post-update-hook.sh
@@ -0,0 +1,18 @@
+sudo tee /opt/cluster.git/hooks/post-update > /dev/null << 'EOF'
+#!/bin/sh
+DATE=$(date +%F)
+CHANGED_MASTER=0
+for ref in "$@"; do
+    if echo "$ref" | grep -q "refs/heads/master"; then
+        CHANGED_MASTER=1
+        break
+    fi
+done
+if [ $CHANGED_MASTER -eq 1 ] || [ -z "$@" ]; then
+    if ! git rev-parse "release-$DATE" >/dev/null 2>&1; then
+        git tag -a "release-$DATE" -m "Release for $DATE"
+    fi
+fi
+EOF
+sudo chmod +x /opt/cluster.git/hooks/post-update
+ENDSSH
